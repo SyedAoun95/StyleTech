97c2e32f7a3d13332f09a63c054d1cb0d2c05dc6
Syedaoun95 <aouns6229@gmail.com>
2025-06-11 18:01:28 +0500

    update

--- src/Components/Client/Client.jsx
+++ src/Components/Client/Client.jsx
@@ -6,30 +6,30 @@ import { ShoppingCart, User } from "lucide-react";
 
 const Client = () => {
     const clientSays = [
-        {
-            id: 1,
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In urna, sit amet eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In urna, sit amet eget.',
-            name: 'John Doe',
-            position: 'CEO, Company',
-        },
-        {
-            id: 2,
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In urna, sit amet eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In urna, sit amet eget.',
-            name: 'John Doe',
-            position: 'CEO, Company',
-        },
-        {
-            id: 3,
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In urna, sit amet eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In urna, sit amet eget.',
-            name: 'John Doe',
-            position: 'CEO, Company',
-        },
-        {
-            id: 4,
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In urna, sit amet eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In urna, sit amet eget.',
-            name: 'John Doe',
-            position: 'CEO, Company',
-        },
+    {
+    id: 1,
+    description: 'I recently had Georgian-style windows and double-glazed windows installed by Style Tech for my house. The service was exceptional—prompt, professional, and the team was a pleasure to work with. The quality of the installation is outstanding; my home feels more comfortable and energy-efficient now. Highly satisfied!',
+    name: 'Umer Hayat',
+    position: 'House Owner',
+  },
+  {
+    id: 2,
+    description: 'As a business owner, I chose Style Tech to install glass windows for my company building. Im impressed with the service—efficient, reliable, and the staff were incredibly supportive. The quality of the windows is top-notch, adding both durability and a modern look to our office. I am truly satisfied!',
+    name: 'Ali Khan',
+    position: 'Business Owner,Lahore',
+  },
+  {
+    id: 3,
+    description: 'I, Sara Ahmed, had glass windows and doors installed for my shop by Style Tech. The service was excellent—timely and well-coordinated. The quality of the materials and installation has transformed my storefront, making it more attractive and secure. I am very pleased with the outcome!',
+    name: 'Sara Ahmed',
+    position: 'Shop owner,Rawalpindi',
+  },
+  {
+   id: 4,
+    description: 'I had laminated glass installed for my stair railings and balconies by Style Tech. The service was outstanding—highly professional and completed on schedule. The quality of the glass is impressive, enhancing both safety and the aesthetic appeal of my home. I’m very satisfied!',
+    name: 'Ahmed Raza',
+    position: 'House onwer,Islamabad(E11)',
+  },
     ];
 
     const settings = {

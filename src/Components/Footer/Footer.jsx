272c187e473d91055b2c6287e3797fd9fe27205e
Syedaoun95 <aouns6229@gmail.com>
2025-06-15 13:18:44 +0500

    update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -39,7 +39,7 @@ const Footer = () => {
                             </div>
                             <p className="text-base text-white font-inter font-normal mb-4 max-w-[350px]">StyleTech Windows is a premium provider of double-glazed and Georgian windows, offering a perfect balance of elegance, efficiency, and endurance. With a commitment to innovation and design, we bring stylish and functional window solutions for every kind of home or project. </p>
                             <div className="footer_social flex items-center gap-3">
-                                <Link className="p-3 rounded-full border-[#007580] inline-block border-[1px]"><Facebook size="1.5rem" color="black" /></Link>
+                                <Link className="p-3 rounded-full border-[#007580] inline-block border-[1px]" to="https://www.facebook.com/share/18u1qqfBsW/"><Facebook size="1.5rem" color="black" /></Link>
                                 <Link className="p-3 inline-block"><AiFillTikTok  size="1.5rem" color="black" /></Link>
                                 <Link className="p-3 inline-block"><Instagram size="1.5rem" color="black" /></Link>
                                 <Link className="p-3 inline-block"><Youtube size="1.5rem" color="black" /></Link>
